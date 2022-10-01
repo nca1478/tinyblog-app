7756893dd00418e975af2f5263a820a855a94038
Nelson Cadenas <nca1478@yahoo.com>
2022-10-01 13:54:40 -0400

    agregado: Componente Showcase en HomePage

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,1 +1,2 @@
+export * from './Showcase'
 export * from './MainNavbar'
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { Showcase } from '../../common'
 
 export const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <>
+      <Showcase />
+    </>
+  )
 }
